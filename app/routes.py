9a7d90ebed8032bbc42e049c3d6045d91012eb85
suminnie <sumin.goh21@gmail.com>
2024-02-22 13:34:04 -0600

    added .flaskenv; updated readme; rendering html template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
+from flask import render_template
 
 @app.route("/")
 @app.route("/index")
 def index():
-	return 'hello'
+    return render_template('main.html', title="Drake Four-Year Plan Generator")
