d0e34646ed751a2e8b6865abb38f417d1d71dfb3
suminnie <sumin.goh21@gmail.com>
2024-03-20 16:08:34 -0500

    example data type and sample random result

--- app/dummy_data.py
+++ app/dummy_data.py
@@ -49,3 +49,66 @@ def random_classes_by_semester():
     for semester in semesters:
         classes_by_semester[semester] = random_sample_classes_per_semester()
     return classes_by_semester
+
+
+majors = [
+    "Anthropology and Sociology",
+    "Astronomy",
+    "Biochemistry, Cell and Molecular Biology",
+    "Biology",
+    "Chemistry",
+    "Computer Science",
+    "Data Analytics",
+    "English",
+    "Environmental Science",
+    "History",
+    "International Relations",
+    "English",
+    "Writing",
+    "Rhetoric and Media Studies",
+    "Environmental Science",
+    "History",
+    "International Relations",
+    "Kinesiology",
+    "Law, Politics and Society",
+    "Mathematics",
+    "Mathematics Education (Secondary)",
+]
+minors = [
+    "Anthropology",
+    "Biology",
+    "Chemistry",
+    "Computer Science",
+    "Cybersecurity",
+    "Data Analytics",
+    "English",
+    "Writing",
+    "Rhetoric and Media Studies",
+    "History",
+    "International Relations",
+    "Mathematics",
+    "Mathematics Education (Secondary)",
+    "Military Studies",
+]
+concentrations = [
+    "Behavior Analysis of Developmental Disabilities",
+    "Biophysics",
+    "Comparative Animal Behavior",
+    "Global and Comparative Public Health",
+    "Human Resources Management",
+    "Interdisciplinary Study of the Humanities and Sciences",
+    "Global and Comparative Public Health",
+    "Human Resources Management",
+    "Interdisciplinary Study of the Humanities and Sciences",
+]
+# complete from this link https://www.drake.edu/academics/undergraduate/majors/ up to "Military Studies" (from "College / School Listing" view)
+drake_curriculum = {
+    "majors": majors,
+    "minors": minors,
+    "concentrations": concentrations,
+}
+user_choices = {
+    "majors": ["Computer Science", "English", "Anthropology and Sociology"],
+    "minors": [],
+    "concentrations": [],
+}
