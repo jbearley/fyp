c514716de6cc04fe7aec85a12bab7de51740e006
Barbara Vrhovac <89888012+BarbaraVrh@users.noreply.github.com>
2024-03-24 16:42:31 -0500

    added some sql tables and printed them out for everyone

--- database.py
+++ database.py
@@ -22,6 +22,116 @@ print("This is the pickone table:")
 for tup in result_2:
     print(tup)
 
+print("\n")
+query_3 = f"""
+SELECT * FROM dbo.BUSINESS_CORE
+"""
+cursor.execute(query_3)
+result_3 = cursor.fetchall()
+print("This is the business core table:")
+for tup in result_3:
+    print(tup)
+
+print("\n")
+query_4 = f"""
+SELECT * FROM dbo.ACCOUNTING_MAJOR
+"""
+cursor.execute(query_4)
+result_4 = cursor.fetchall()
+print("This is the accounting major table:")
+for tup in result_4:
+    print(tup)
+
+print("\n")
+query_5 = f"""
+SELECT * FROM dbo.PICK_TWO_ACC
+"""
+cursor.execute(query_5)
+result_5 = cursor.fetchall()
+print("This is the pick two accounting table:")
+for tup in result_5:
+    print(tup)
+
+print("\n")
+query_6 = f"""
+SELECT * FROM dbo.ACT_SCI_MAJOR
+"""
+cursor.execute(query_6)
+result_6 = cursor.fetchall()
+print("This is the actuarial science major table:")
+for tup in result_6:
+    print(tup)
+
+print("\n")
+query_7 = f"""
+SELECT * FROM dbo.PICK_TWO_ACT_SCI
+"""
+cursor.execute(query_7)
+result_7 = cursor.fetchall()
+print("This is the pick two actuarial science table:")
+for tup in result_7:
+    print(tup)
+
+print("\n")
+query_8 = f"""
+SELECT * FROM dbo.BLAW_MAJOR
+"""
+cursor.execute(query_8)
+result_8 = cursor.fetchall()
+print("This is the business law major table:")
+for tup in result_8:
+    print(tup)
+
+print("\n")
+query_9 = f"""
+SELECT * FROM dbo.CHOOSE_THREE_BLAW
+"""
+cursor.execute(query_9)
+result_9 = cursor.fetchall()
+print("This is the choose three business law table:")
+for tup in result_9:
+    print(tup)
+
+print("\n")
+query_10 = f"""
+SELECT * FROM dbo.FIRST_BL_PICK_ONE
+"""
+cursor.execute(query_10)
+result_10 = cursor.fetchall()
+print("This is the first business law pick one table:")
+for tup in result_10:
+    print(tup)
+
+print("\n")
+query_11 = f"""
+SELECT * FROM dbo.SECOND_BL_PICK_ONE
+"""
+cursor.execute(query_11)
+result_11 = cursor.fetchall()
+print("This is the second business law pick one table:")
+for tup in result_11:
+    print(tup)
+
+print("\n")
+query_12 = f"""
+SELECT * FROM dbo.ECON_MAJOR
+"""
+cursor.execute(query_12)
+result_12 = cursor.fetchall()
+print("This is the econ major table:")
+for tup in result_12:
+    print(tup)
+
+print("\n")
+query_13 = f"""
+SELECT * FROM dbo.CHOOSE_FOUR_ECON
+"""
+cursor.execute(query_13)
+result_13 = cursor.fetchall()
+print("This is the choose four econ table:")
+for tup in result_13:
+    print(tup)
+
 
 conn.commit()
 cursor.close()
