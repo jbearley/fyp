3e4dc51334001fd8fbb55adcae1514a0e3b3302d
suminnie <sumin.goh21@gmail.com>
2024-04-01 15:46:50 -0500

    added example requirements table

--- app/example_data.py
+++ app/example_data.py
@@ -1,371 +1,43 @@
 """
+FYP Table
 {
-	"semester_key": [
-		<dictionary representation of a class>,
-		<dictionary representation of a class>,
-		<dictionary representation of a class>,
-		...
-	],
-	"semester_key": [
-		<dictionary representation of a class>,
-		<dictionary representation of a class>,
-		<dictionary representation of a class>,
-		...
-	],
+	"semester_key_1": {
+		"class_key_1": <class>,
+		"class_key_2": <class>,
+		"class_key_3": <class>,
+		"class_key_4": <class>,
+		"class_key_5": <class>,
+	},
 	...
 }
 
-Full random four-year plan below
-"""
-
+Requirements Table (I think this is important so that we can justify/explain the FYP result on the front-end)
 {
-    "2022 Fall": [
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Critical Thinking",
-                "Written Communication",
-                "First Year Seminar",
-                "Scientific Literacy - Physical Science",
-                "Information Literacy",
-                "Quantitative Literacy",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": ["Critical Thinking"],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Information Literacy",
-                "Equity and Inclusion",
-                "Historical Foundations",
-                "Written Communication",
-                "Scientific Literacy - Life/Behavioral Science",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Critical Thinking",
-                "Information Literacy",
-                "Global and Cultural Understanding",
-                "Quantitative Literacy",
-                "First Year Seminar",
-            ],
-        },
-    ],
-    "2023 Spring": [
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Information Literacy",
-                "Scientific Literacy - Physical Science",
-                "Historical Foundations",
-                "The Engaged Citizen",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": ["Information Literacy", "The Engaged Citizen"],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Global and Cultural Understanding",
-                "Critical Thinking",
-                "Equity and Inclusion",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Global and Cultural Understanding",
-                "Information Literacy",
-                "Values and Ethics",
-                "Scientific Literacy - Life/Behavioral Science",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Historical Foundations",
-                "Written Communication",
-                "Artistic Literacy",
-                "Values and Ethics",
-                "First Year Seminar",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "The Engaged Citizen",
-                "Quantitative Literacy",
-                "Critical Thinking",
-                "Artistic Literacy",
-            ],
-        },
-    ],
-    "2023 Fall": [
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Scientific Literacy - Life/Behavioral Science",
-                "Written Communication",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": ["Global and Cultural Understanding", "Values and Ethics"],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "First Year Seminar",
-                "Scientific Literacy - Physical Science",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Artistic Literacy",
-                "Information Literacy",
-                "Scientific Literacy - Life/Behavioral Science",
-                "First Year Seminar",
-                "Quantitative Literacy",
-            ],
-        },
-    ],
-    "2024 Spring": [
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Artistic Literacy",
-                "Scientific Literacy - Life/Behavioral Science",
-                "The Engaged Citizen",
-                "Historical Foundations",
-                "Critical Thinking",
-                "Equity and Inclusion",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "First Year Seminar",
-                "Written Communication",
-                "Critical Thinking",
-                "Artistic Literacy",
-                "Scientific Literacy - Life/Behavioral Science",
-                "Equity and Inclusion",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Historical Foundations",
-                "Scientific Literacy - Physical Science",
-                "Critical Thinking",
-                "Written Communication",
-                "Scientific Literacy - Life/Behavioral Science",
-                "Quantitative Literacy",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Information Literacy",
-                "Artistic Literacy",
-                "Scientific Literacy - Physical Science",
-            ],
-        },
-    ],
-    "2024 Fall": [
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Critical Thinking",
-                "Equity and Inclusion",
-                "Scientific Literacy - Physical Science",
-                "Information Literacy",
-                "Artistic Literacy",
-                "Scientific Literacy - Life/Behavioral Science",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Written Communication",
-                "Equity and Inclusion",
-                "Quantitative Literacy",
-                "Values and Ethics",
-                "Critical Thinking",
-                "Artistic Literacy",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Written Communication",
-                "Equity and Inclusion",
-                "Information Literacy",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "First Year Seminar",
-                "The Engaged Citizen",
-                "Values and Ethics",
-            ],
-        },
-    ],
-    "2025 Spring": [
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Historical Foundations",
-                "The Engaged Citizen",
-                "Artistic Literacy",
-                "Values and Ethics",
-                "Information Literacy",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Equity and Inclusion",
-                "Quantitative Literacy",
-                "The Engaged Citizen",
-                "Values and Ethics",
-                "Scientific Literacy - Physical Science",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": ["Global and Cultural Understanding", "Equity and Inclusion"],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Information Literacy",
-                "Historical Foundations",
-                "Critical Thinking",
-                "Scientific Literacy - Life/Behavioral Science",
-            ],
-        },
-    ],
-    "2025 Fall": [
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Scientific Literacy - Physical Science",
-                "Values and Ethics",
-                "Scientific Literacy - Life/Behavioral Science",
-                "The Engaged Citizen",
-                "Quantitative Literacy",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "First Year Seminar",
-                "Values and Ethics",
-                "Critical Thinking",
-                "The Engaged Citizen",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": ["Artistic Literacy"],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": ["Scientific Literacy - Physical Science"],
-        },
-    ],
-    "2026 Spring": [
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Written Communication",
-                "Quantitative Literacy",
-                "Scientific Literacy - Physical Science",
-                "Values and Ethics",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Scientific Literacy - Life/Behavioral Science",
-                "Information Literacy",
-                "First Year Seminar",
-                "Equity and Inclusion",
-                "Scientific Literacy - Physical Science",
-                "Quantitative Literacy",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": ["Historical Foundations", "First Year Seminar"],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Equity and Inclusion",
-                "First Year Seminar",
-                "Written Communication",
-            ],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": ["Global and Cultural Understanding"],
-        },
-        {
-            "title": "Class Name",
-            "course_number": "CLS 123",
-            "attributes": [
-                "Values and Ethics",
-                "Written Communication",
-                "Critical Thinking",
-                "The Engaged Citizen",
-                "Equity and Inclusion",
-            ],
-        },
-    ],
+	"major_1": {
+        "singles": [
+            "class_key_1",
+            "class_key_2",
+            ...
+        ],
+        "pick_one_1": [
+            "class_key_3",
+            "class_key_4",
+            ...
+        ],
+        "pick_one_2": [
+            "class_key_5",
+            "class_key_6",
+            ...
+        ],
+        "pick_two_1": [
+            "class_key_7",
+            "class_key_8",
+            "class_key_9",
+            "class_key_10",
+            ...
+        ],
+    },
+    "major_2": <major 2 requirements, similar to above>,
+    "AOIs": <requirements, similarly indicating any pick-ones etc.>
 }
+"""
