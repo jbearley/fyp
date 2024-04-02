f30735ff6ddad466b3549044cda5c03706849541
annebelle.dennis@drake.edu <annebelle.dennis@drake.edu>
2024-04-01 22:43:08 -0500

    added dict8 to dictionaries2 file, added fall/spring offerings as a criteria for placement

--- finalizeSchedule.py
+++ finalizeSchedule.py
@@ -1,4 +1,10 @@
 from placement_algorithm import *
+from Jacob import *
+from collections import OrderedDict
+from operator import itemgetter
+from class_prereqs_score import class_prereqs_score
+from class_is_prereq_score import class_is_prereq_score
+from dictionaries2 import *
 
 def addSemester(pastSemName):
     if "Fall " in pastSemName:
@@ -19,11 +25,12 @@ dict_2 = dictionaries[1]
 dict_3 = dictionaries[2]
 dict_4 = dictionaries[3]
 dict_5 = dictionaries[4]
-dict_7 = dictionaries[5]
-dict_8 = dictionaries[6]
-semesterList = placement_algorithm(dict_1)
+dict_6 = dictionaries[5]
+dict_7 = dictionaries[6]
+dict_8 = dictionaries[7]
+semesterList = placement_algorithm(dict_1, dict_2, startingSemester)
 
-semesterList = [0, ["ACTS 50", "ACTS 131"], ["ACTS 161"], [], [], [], [], [], []]
+#semesterList = [0, ["MATH 50", "ACTS 50", "ACTS 131"], ["ACTS 161"], [], [], [], [], [], []]
 
 def finalCheck(dict_2, dict_7, dict_8, startingSemester, semesterList):
     working = True
@@ -38,7 +45,7 @@ def finalCheck(dict_2, dict_7, dict_8, startingSemester, semesterList):
                 currentYear = int(currentSemester.replace("Spring ", ""))
             for semesterClass in oneSemesterList: #for each class in the selected semester
                 #Confirm that classes are placed when they are offered.
-                if semesterClass == "AOI": #ignore AOIs - may need to update this depending on how we end up formatting these
+                if semesterClass == "Placeholder" or semesterClass == "AOI": #ignore AOIs - may need to update this depending on how we end up formatting these
                     continue
                 #check fall/spring
                 if currentSeason == "Fall":
@@ -72,8 +79,11 @@ def reformat(semesterList, startingSemester, dict_8):
     while i <= 8:
         tempList = []
         for course in semesterList[i]:
-            tempList.append(dict_8[course]) #gets dictionary for course with all info we pass tp frontend
-        finalSchedule[semester] = tempList #add entry for the semester
+            if course != "Placeholder" and course != "AOI":
+                tempList.append(dict_8[course]) #gets dictionary for course with all info we pass to frontend
+            else:
+                tempList.append("Placeholder")
+        finalSchedule[semester] = tempList #add entry for this semester
         
         semester = addSemester(semester)
         i+=1
--- placement_algorithm.py
+++ placement_algorithm.py
@@ -5,7 +5,7 @@ from class_is_prereq_score import class_is_prereq_score
 from dictionaries2 import *
 
 
-def placement_algorithm(requirements):
+def placement_algorithm(requirements, dict_2, startingSemester):
     prereq_dict = class_prereqs_score(requirements)
     postreq_dict = (class_is_prereq_score(requirements))[0]
 
@@ -17,9 +17,19 @@ def placement_algorithm(requirements):
         fits_arr = []
         while fit <=8:
             if fit >= prereq_score and fit <= 8-postreq_score+1:
-                fits_arr.append(fit)
+                if "Fall" in startingSemester:
+                    if int(dict_2[a][0])==1 and fit % 2 != 0: #offered fall, fall semester
+                        fits_arr.append(fit) #that works
+                    elif int(dict_2[a][1])==1 and fit % 2 == 0: #offered spring, spring semester
+                        fits_arr.append(fit) #that works
+                else:
+                    if int(dict_2[a][0])==1 and fit % 2 == 0: #offered fall, fall semester
+                        fits_arr.append(fit) #that works
+                    elif int(dict_2[a][1])==1 and fit % 2 != 0: #offered spring, spring semester
+                        fits_arr.append(fit) #that works
             fit += 1
         spots_dict[a] = fits_arr
+    #print(spots_dict)
         #here would be where you check class requirements and also incorporate that info?
 
     spots_dict = sorted(spots_dict.items(), key=itemgetter(1))
@@ -34,7 +44,7 @@ def placement_algorithm(requirements):
             if j in spots_dict[course]:
                 i=1
                 while i <= 8:
-                    if creditsLeft[i] != 0:   
+                    if creditsLeft[i] != 0:  
                         good = True
                         for a in semesterLists[i]:
                             #if those aren't pre-reqs of course, continue
@@ -60,4 +70,8 @@ def placement_algorithm(requirements):
 
     return semesterLists
 
-
+# dictionaries = createDictionaries("Actuarial Science")
+# dict_1 = dictionaries[0]
+# dict_2 = dictionaries[1]
+# print(dict_2, "\n")
+# print(placement_algorithm(dict_1, dict_2, "Fall 2022"))
