af6e8f7de1726a053697ad9d7ac04d401032a021
annebelle.dennis@drake.edu <annebelle.dennis@drake.edu>
2024-04-18 00:43:56 -0500

    resolved what would've been merge conflicts

--- dictionaries2.py
+++ dictionaries2.py
@@ -1,25 +1,25 @@
 from azuresqlconnector import *
+from OrRequirements import *
 
 
-
-def createDictionaries(selectedMajor):
+def createDictionaries(selectedMajor1):
     conn = SQLConnection()
     conn = conn.getConnection()
     cursor = conn.cursor()
-    #this will have to be done for all majors
-    if selectedMajor == "ACTUARIAL SCIENCE":
-        selectedMajor =  "ACT_SCI_MAJOR"
-    elif selectedMajor== "Accounting":
-        selectedMajor="ACCOUNTING_MAJOR"
-    elif selectedMajor=="Business Law":
-        selectedMajor="BLAW_MAJOR"
-    elif selectedMajor=="Economics":
-        selectedMajor="ECON_MAJOR"
+#this will have to be done for all majors
+    if selectedMajor1 == "ACTUARIAL SCIENCE":
+        selectedMajor = "ACT_SCI_MAJOR"
+    elif selectedMajor1 == "ACCOUNTING":
+        selectedMajor = "ACCOUNTING_MAJOR"
+    elif selectedMajor1 == "ECONOMICS":
+        selectedMajor = "ECON_MAJOR"
+    elif selectedMajor1 == "BUSINESS LAW":
+        selectedMajor = "BLAW_MAJOR"
     
 
 
     #dictionary with Class Id for the key, Prerequisites for the value
-    query_1 = "SELECT CLASSES.ClassID, Prereqs FROM dbo.CLASSES, dbo." + selectedMajor + " WHERE CLASSES.ClassID =" + selectedMajor + ".ClassID"
+    query_1 = "SELECT CLASSES.ClassID, Prereqs FROM dbo.CLASSES, dbo.BUSINESS_CORE, dbo." + selectedMajor + " WHERE CLASSES.ClassID =" + selectedMajor + ".ClassID OR CLASSES.ClassID = BUSINESS_CORE.ClassID"
     cursor.execute(query_1)
     dict_1 = {}
     result = cursor.fetchall()
@@ -29,11 +29,23 @@ def createDictionaries(selectedMajor):
             if a[0] == " ":
                 a = a[1:]
         dict_1[tup[0]]=tup[1]
-    dict_1["ACTS 140"] = ["ACTS 135"]
-    #print(dict_1, "/n/n")
+    if "FIN 101" in dict_1:
+        dict_1["FIN 101"] = ["ACCT 42", "IS 44", "ECON 02", "STAT 71/STAT 130/ACTS 131"]
+    if "MGMT 120" in dict_1:
+        dict_1["MGMT 120"] = ["STAT 72/ACTS 135"]
+    if "ECON 190" in dict_1:
+        dict_1["ECON 190"] = ["ECON 170/STAT 170"]
+    dict_1 = dealWithOrReqs(dict_1)
+    if "ACTS 140" in dict_1 and "ACT 135" in dict_1["ACTS 140"]:
+        dict_1["ACTS 140"].remove("ACT 135")
+        dict_1["ACTS 140"].append("ACTS 135") 
+    if "ECON 170" in dict_1 and "MATH 28" in dict_1["ECON 170"]:
+        dict_1["ECON 170"].remove("MATH 28") #what curriculum is the econ major stuff from?
+    #print("major:", selectedMajor)
+    #(dict_1, "\n\n")
 
     #dictionary with ClassID for the key, value consists of a concatenated string of Fall (0 or 1) and Spring (0 or 1) value depending on whether the class is offered in the fall / spring or not
-    query_2 = "SELECT CLASSES.ClassID, Fall, Spring FROM dbo.CLASSES, dbo." + selectedMajor + " WHERE CLASSES.ClassID =" + selectedMajor + ".ClassID"
+    query_2 = "SELECT CLASSES.ClassID, Fall, Spring FROM dbo.CLASSES"#, dbo." + selectedMajor + " WHERE CLASSES.ClassID =" + selectedMajor + ".ClassID"
     cursor.execute(query_2)
     dict_2 = {}
     result_2 = cursor.fetchall()
@@ -42,7 +54,7 @@ def createDictionaries(selectedMajor):
     #print(dict_2)
 
     #dictionary with ClassID for the key, number of credits for the value
-    query_3 = "SELECT CLASSES.ClassID, Credits FROM dbo.CLASSES, dbo." + selectedMajor + " WHERE CLASSES.ClassID =" + selectedMajor + ".ClassID"
+    query_3 = "SELECT CLASSES.ClassID, Credits FROM dbo.CLASSES"#, dbo." + selectedMajor + " WHERE CLASSES.ClassID =" + selectedMajor + ".ClassID"
     cursor.execute(query_3)
     dict_3 = {}
     result_3 = cursor.fetchall()
@@ -50,7 +62,7 @@ def createDictionaries(selectedMajor):
         dict_3[tup[0]]=tup[1]
 
     #dictionary with ClassID for the key, grade requirement for the value. If no grade requirement, than it has None.
-    query_4 = "SELECT CLASSES.ClassID, GradeReq FROM dbo.CLASSES, dbo." + selectedMajor + " WHERE CLASSES.ClassID =" + selectedMajor + ".ClassID"
+    query_4 = "SELECT CLASSES.ClassID, GradeReq FROM dbo.CLASSES"#, dbo." + selectedMajor + " WHERE CLASSES.ClassID =" + selectedMajor + ".ClassID"
     cursor.execute(query_4)
     dict_4 = {}
     result_4 = cursor.fetchall()
@@ -58,7 +70,7 @@ def createDictionaries(selectedMajor):
         dict_4[tup[0]]=tup[1]
 
     #dictionary with ClassID for the key, corequisities for the value. If no corequisities, then the value is None.
-    query_5 = "SELECT CLASSES.ClassID, Coreq FROM dbo.CLASSES, dbo." + selectedMajor + " WHERE CLASSES.ClassID =" + selectedMajor + ".ClassID"
+    query_5 = "SELECT CLASSES.ClassID, Coreq FROM dbo.CLASSES"#, dbo." + selectedMajor + " WHERE CLASSES.ClassID =" + selectedMajor + ".ClassID"
     cursor.execute(query_5)
     dict_5 = {}
     result_5 = cursor.fetchall()
@@ -86,7 +98,7 @@ def createDictionaries(selectedMajor):
         dict_6[tup[0]]= tup[1] 
         
     #dictionary with ClassID for the key, value consists of a concatenated string of Odd (0 or 1) and Even (0 or 1) value depending on whether the class is offered in odd / even years or not
-    query_7 = "SELECT CLASSES.ClassID, Odd, Even FROM dbo.CLASSES, dbo." + selectedMajor + " WHERE CLASSES.ClassID =" + selectedMajor + ".ClassID"
+    query_7 = "SELECT CLASSES.ClassID, Odd, Even FROM dbo.CLASSES"#, dbo." + selectedMajor + " WHERE CLASSES.ClassID =" + selectedMajor + ".ClassID"
     cursor.execute(query_7)
     dict_7 = {}
     result_7 = cursor.fetchall()
@@ -94,7 +106,7 @@ def createDictionaries(selectedMajor):
         dict_7[tup[0]]=str(tup[1])+str(tup[2])
 
     #dictionary with ClassID for the key, value is a list of courseId, course title, credits, and aoi attributes
-    query_8 = "SELECT CLASSES.ClassID, Credits FROM dbo.CLASSES, dbo." + selectedMajor + " WHERE CLASSES.ClassID =" + selectedMajor + ".ClassID"
+    query_8 = "SELECT CLASSES.ClassID, Credits FROM dbo.CLASSES"#, dbo." + selectedMajor + " WHERE CLASSES.ClassID =" + selectedMajor + ".ClassID"
     cursor.execute(query_8)
     dict_8 = {}
     result_8 = cursor.fetchall()
@@ -105,7 +117,7 @@ def createDictionaries(selectedMajor):
             aois = 'None'
         dict_8[tup[0]]=[tup[0], 'course title', float(tup[1]), aois] #update when DB has those capabilities
     #dictionary with ClassID for the key, value is the class Name
-    query_9 = "SELECT CLASSES.ClassID, CLASSES.ClassName FROM dbo.CLASSES, dbo." + selectedMajor + " WHERE CLASSES.ClassID =" + selectedMajor + ".ClassID"
+    query_9 = "SELECT CLASSES.ClassID, CLASSES.ClassName FROM dbo.CLASSES"#, dbo." + selectedMajor + " WHERE CLASSES.ClassID =" + selectedMajor + ".ClassID"
     cursor.execute(query_9)
     dict_9={}
     result_9= cursor.fetchall()
